0fccb63081bd7ba658ffd81e417fb4d3452c224e
陈高星 <13302010055@fudan.edu.cn>
2017-10-11 18:27:29 +0800

    13302010055@fudan.edu.cn

--- src/main/java/luna/filter/BulkElasticsearchFilter.java
+++ src/main/java/luna/filter/BulkElasticsearchFilter.java
@@ -51,7 +51,7 @@ public class BulkElasticsearchFilter  extends BaseFilter{
 		}
 		long getDataTimeMillis = System.currentTimeMillis();
         String modify_time =(String)payload.get("modify_time");
-		long modifyTimeMillis = TimeUtil.stringToLong(modify_time,"yy-MM-dd HH:mm:ss");
+		long modifyTimeMillis = TimeUtil.stringToLong(modify_time,"yy-MM-dd HH:mm:ss.SSS");
 		long diffMillis = getDataTimeMillis - modifyTimeMillis;
 		logTime.info(""+table+" "+diffMillis);
 	}
