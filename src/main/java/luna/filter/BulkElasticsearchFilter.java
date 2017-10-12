08ee3c788839ca41d749eac55e172eddf635d7a4
root <root@es-14-88.dev.xesv5.com>
2017-10-12 18:13:21 +0800

    13302010055@fudan.edu.cn

--- src/main/java/luna/filter/BulkElasticsearchFilter.java
+++ src/main/java/luna/filter/BulkElasticsearchFilter.java
@@ -53,7 +53,7 @@ public class BulkElasticsearchFilter  extends BaseFilter{
 		long getDataTimeMillis = System.currentTimeMillis();
         String modify_time =(String)payload.get("modify_time");
         long modifyTimeMillis=0;
-        modifyTimeMillis = TimeUtil.stringToLong(modify_time, "yy-MM-dd HH:mm:ss");
+        modifyTimeMillis = TimeUtil.stringToLong(modify_time, "yy-MM-dd HH:mm:ss.SSS");
 		long diffMillis = getDataTimeMillis - modifyTimeMillis-28800000;
 		logTime.info(""+table+" "+diffMillis);
 	}
